c7f984e598c3f3d12b5e78b9c706d41ad5e3dd32
subaroon <subaru.fukuda@gmail.com>
2024-02-21 01:18:33 +0900

    basic03 assignment1 improvement

--- basic03-map-counter/xdp_prog_kern.c
+++ basic03-map-counter/xdp_prog_kern.c
@@ -25,8 +25,8 @@ struct {
 SEC("xdp")
 int  xdp_stats1_func(struct xdp_md *ctx)
 {
-	// void *data_end = (void *)(long)ctx->data_end;
-	// void *data     = (void *)(long)ctx->data;
+	void *data_end = (void *)(long)ctx->data_end;
+	void *data     = (void *)(long)ctx->data;
 	struct datarec *rec;
 	__u32 key = XDP_PASS; /* XDP_PASS = 2 */
 
@@ -39,18 +39,14 @@ int  xdp_stats1_func(struct xdp_md *ctx)
 	if (!rec)
 		return XDP_ABORTED;
 
-	void *data_end = (void *)(long)ctx->data_end;
-	void *data     = (void *)(long)ctx->data;
 	__u64 bytes = data_end - data; /* Calculate packet length */
-	lock_xadd(&rec->rx_bytes, bytes);
 
 	/* Multiple CPUs can access data record. Thus, the accounting needs to
 	 * use an atomic operation.
 	 */
 	lock_xadd(&rec->rx_packets, 1);
-        /* Assignment#1: Add byte counters
-         * - Hint look at struct xdp_md *ctx (copied below)
-         *
+	lock_xadd(&rec->rx_bytes, bytes);
+        /*
          * Assignment#3: Avoid the atomic operation
          * - Hint there is a map type named BPF_MAP_TYPE_PERCPU_ARRAY
          */
